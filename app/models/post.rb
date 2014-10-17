e24c03372a39b2dd1ffa690496ae7f49a3f24245
Eldwick <jamesjalandoni1@gmail.com>
2014-10-16 21:05:32 -0400

    Added search functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,10 @@ class Post < ActiveRecord::Base
       time = Time.at(dbTime)
       time.to_formatted_s(:long)
   end
+
+  def self.search(query)
+    # where(:title, query) -> This would return an exact match of the query
+    where("lower(title) like ? OR lower(body) like ?", "%#{query.downcase}%", "%#{query.downcase}%")
+  end
+
 end
